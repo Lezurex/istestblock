4c47747771186b2a6cd78928aa733e7aea4efe6e
Lenny <lenny.angst@easyid.ch>
2021-09-28 09:34:27 +0200

    first commit

---
+++ istest.js
@@ -0,0 +1,3 @@
+window.addEventListener("mousemove", event => event.stopImmediatePropagation(), true);
+window.addEventListener("keyup", event => event.stopImmediatePropagation(), true);
+window.addEventListener("keydown", event => event.stopImmediatePropagation(), true);
